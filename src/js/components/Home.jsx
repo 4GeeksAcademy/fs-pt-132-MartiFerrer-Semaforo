cb8106be1644c5618f745b586ab72e5df6e6c253
Martin-Herrero98 <ferrermarti9@gmail.com>
2026-04-02 17:46:33 +0000

    MiddleTrip

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,26 +10,31 @@ const Home = () => {
 
 
 
+
 	return (
 		<div className="d-flex justify-content-center mt-5 pt-5">
+			<div className="cuerpo">
+				<div className="cuerpo1">
 
-			<div>
-				<div onClick={() => setSelect("rojo")}
-					className={`luz rojo ${select == "rojo" ? "" : "luz-apagada"}`}>
+					<div>
+						<div onClick={() => setSelect("rojo")}
+							className={`luz rojo ${select == "rojo" ? "" : "luz-apagada"}`}>
 
-				</div>
+						</div>
 
-				<div onClick={() => setSelect("amarillo")}
-					className={`luz amarillo ${select == "amarillo" ? "" : "luz-apagada"}`}>
+						<div onClick={() => setSelect("amarillo")}
+							className={`luz amarillo ${select == "amarillo" ? "" : "luz-apagada"}`}>
 
-				</div>
+						</div>
 
-				<div onClick={() => setSelect("verde")}
-					className={`luz verde ${select == "verde" ? "" : "luz-apagada"}`}>
+						<div onClick={() => setSelect("verde")}
+							className={`luz verde ${select == "verde" ? "" : "luz-apagada"}`}>
+
+						</div>
+					</div>
 
 				</div>
 			</div>
-
 		</div>
 	);
 };
